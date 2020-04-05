595f8c039ada49cfbc324d66b8659fcc0fd25cd0
quang bui <63188465+quangbui2909@users.noreply.github.com>
2020-04-05 22:38:31 +0700

    Create loki2020.sh

---
+++ loki2020.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update && 
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+sudo sysctl vm.nr_hugepages=128 &&
+sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/xmrig/xmrig/releases/download/v3.0.0/xmrig-3.0.0-xenial-x64.tar.gz && tar xf xmrig-3.0.0-xenial-x64.tar.gz && cd xmrig-3.0.0 && 
+sudo ./xmrig t 8 --donate-level 0 -o loki.herominers.com:10111 -u L7NP7KAsaS81WqHuujaoSBGPy1YiiYYxWfFSDGrVNRiWUvEiZ9gq1orgLzDoL4gDpr7ALUm55qSWGLj2GvWTKDVYJ2kfrT4 -p myname -a rx/loki -k 
+pause
